48731ccb6636ffcc052127a7885dfd1e1b8b319d
HusseinYoussef <sonamosa21@gmail.com>
2022-05-17 19:01:39 +0200

    Added Exception_Handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    # Exception Handlers
+    rescue_from ActiveRecord::RecordNotFound do |e|
+        render json: {message: e.message}, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+        render json: {message: e.message}, status: :unprocessable_entity
+    end
 end
