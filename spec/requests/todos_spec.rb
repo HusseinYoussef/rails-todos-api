66153a847804f6812b43fbeccd7f5b0730e4aba9
HusseinYoussef <sonamosa21@gmail.com>
2022-05-19 19:23:49 +0200

    Fix a test case for Todos specs

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -121,7 +121,7 @@ RSpec.describe "Todos API", type: :request do
   describe "DELETE /api/v1/todos/id" do
     
     context "when the record doesn't exist" do
-      let(:todo_id) {100}
+      let(:todo_id) {todos.last.id+1}
       before {delete "/api/v1/todos/#{todo_id}"}
   
       it "returns status code 404" do
